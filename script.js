8b56a937cf8260eebcd001e4f360488ab24a2fb5
osakeneko <comradecatlin@gmail.com>
2025-05-25 03:38:08 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -29,12 +29,20 @@ setInterval(updateClock, 1000);
 updateClock();
 const clockContainer = document.getElementById('clock-container');
 const clock = document.getElementById('clock');
-const infoP = document.createElement('p');
-infoP.textContent = "klik aku jika ingin kembali";
-infoP.style.color = "#03fa6e";
-infoP.style.marginTop = "8px";
-infoP.style.cursor = "pointer";
-infoP.onclick = function() {
-    window.location.href = "https://osakeneko.github.io/kelas-rpl/index.html";
+const infoSvg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+infoSvg.setAttribute("viewBox", "0 0 32 32");
+infoSvg.setAttribute("width", "32");
+infoSvg.setAttribute("height", "32");
+infoSvg.setAttribute("fill", "#03fa6e");
+infoSvg.style.cursor = "pointer";
+infoSvg.style.marginTop = "8px";
+infoSvg.innerHTML = `
+  <g>
+    <path d="M16 2.672l-5.331 5.331v-2.133h-4.265v6.398l-3.755 3.755 0.754 0.754 12.597-12.597 12.597 12.597 0.754-0.754-13.351-13.351zM7.47 6.937h2.132v2.132l-2.133 2.133v-4.265z"/>
+    <path d="M6.404 16.533v12.795h7.464v-7.464h4.265v7.464h7.464v-12.795l-9.596-9.596-9.596 9.596zM24.53 28.262h-5.331v-7.464h-6.398v7.464h-5.331v-11.287l8.53-8.53 8.53 8.53v11.287z"/>
+  </g>
+`;
+infoSvg.onclick = function() {
+    window.location.href = "index.html";
 };
-clockContainer.appendChild(infoP);
+clockContainer.appendChild(infoSvg);
